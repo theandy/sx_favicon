7c0ed1b813e0e7e0d07dcccf26fa9476d78a2715
andreasloewer <info@andreasloewer.de>
2025-09-19 14:03:38 +0200

    no message

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,30 +1,38 @@
 <?php
 defined('TYPO3') || die();
-error_log('[sx_favicon] ext_localconf was executed');
 
 call_user_func(function () {
-    // FE Middleware: stabile Root-Pfade wie /favicon.ico bedienen
+    // DEBUG: Marker – ext_localconf wird ausgeführt
+    error_log('[sx_favicon] ext_localconf start');
+
+    // FE Middleware …
     $GLOBALS['TYPO3_CONF_VARS']['FE']['middlewares']['sx_favicon/favicon'] = [
         'target' => \AndreasLoewer\SxFavicon\Middleware\FaviconMiddleware::class,
         'after' => ['typo3/cms-frontend/base-redirect-resolver'],
         'before' => ['typo3/cms-frontend/static-route-resolver'],
     ];
 
-    // Backend-Modul (unter Site Management)
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-        'SxFavicon',
-        'site',
-        'favicons',
-        '',
-        [\AndreasLoewer\SxFavicon\Controller\ConfigController::class => 'index,save'],
-        [
-            'access' => 'admin',
-            'icon' => 'EXT:sx_favicon/Resources/Public/Icons/module.svg',
-            'labels' => 'LLL:EXT:sx_favicon/Resources/Private/Language/locallang_mod.xlf'
-        ]
-    );
+    try {
+        // Backend-Modul (unter Site Management)
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+            'SxFavicon',
+            'site',
+            'favicons',
+            '',
+            [\AndreasLoewer\SxFavicon\Controller\ConfigController::class => 'index,save'],
+            [
+                'access' => 'admin',
+                'icon' => 'EXT:sx_favicon/Resources/Public/Icons/module.svg',
+                'labels' => 'LLL:EXT:sx_favicon/Resources/Private/Language/locallang_mod.xlf'
+            ]
+        );
+        error_log('[sx_favicon] registerModule OK');
+    } catch (\Throwable $e) {
+        error_log('[sx_favicon] registerModule FAILED: ' . $e->getMessage());
+    }
 
     // Fluid ViewHelper Namespace
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['sxFavicon'][] =
         'AndreasLoewer\\SxFavicon\\ViewHelpers';
+    error_log('[sx_favicon] ext_localconf end');
 });
