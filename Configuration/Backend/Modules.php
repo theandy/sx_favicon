ac37ea3cd9c1444061e0a03b9b4a3f4a7df075f9
andreasloewer <info@andreasloewer.de>
2025-09-23 09:50:22 +0200

    no message

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -4,23 +4,16 @@ declare(strict_types=1);
 use AndreasLoewer\SxFavicon\Controller\ConfigController;
 
 return [
-    'site_favicons' => [
-        'parent' => 'site',                    // Site Management
+    'site_favicons_v2' => [              // <— NEUER KEY
+        'parent' => 'site',
         'access' => 'admin',
         'workspaces' => 'live',
-        'path' => '/module/site/favicons',
+        'path' => '/module/site/sxfv',     // <— NEUER PFAD
         'iconIdentifier' => 'module-sx-favicon',
         'labels' => 'LLL:EXT:sx_favicon/Resources/Private/Language/locallang_mod.xlf',
-
-        // nur Symfony-Routen, KEIN Extbase
         'routes' => [
-            '_default' => [
-                'target' => ConfigController::class . '::index',
-            ],
-            'save' => [
-                'path'   => '/save',
-                'target' => ConfigController::class . '::save',
-            ],
+            '_default' => ['target' => ConfigController::class.'::index'],
+            'save'     => ['path'=>'/save','target'=>ConfigController::class.'::save'],
         ],
     ],
 ];
